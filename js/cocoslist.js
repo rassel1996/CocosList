1b5bf2d12d12e3426bd1d2ac6032220f6347ab49
ubazinga <rassel1996@gmail.com>
2015-11-27 06:25:45 +0200

    banana

--- js/cocoslist.js
+++ js/cocoslist.js
@@ -1,12 +1,12 @@
 var CocosList = function() {
     this.constructor = function(options) {
-        console.info(options);
+        this.options = options;
 
         this.render();
     }
 
     this.render = function() {
-        var select = document.getElementById('cocoslist');
+        var select = this.options.element;
         select.classList.add('cocoslist__hide');
 
         var cocoslistElements = document.createElement('div');
@@ -20,67 +20,65 @@ var CocosList = function() {
             }
         });
 
-        document.onclick = function( event ) {
-            if(event.target.className.indexOf('cocoslist-element') == -1) {
+        document.addEventListener('click', function(event) {
+            if(!event.target.classList.contains('cocoslist-element')) {
                 activeElements = document.getElementsByClassName('cocoslist-elements__active');
 
                 if(activeElements.length > 1) {
                     [].forEach.call(activeElements, function(index, element) {
-                        clases = element.className.replace("cocoslist-elements__active", "");
-                        element.className = clases.trim();
+                        element.classList.remove("cocoslist-elements__active");
                     });
                 } else {
                     try {
                         if(activeElements[0].hasAttribute("class")) {
-                            clases = activeElements[0].className.replace("cocoslist-elements__active", "");
-                            activeElements[0].className = clases.trim();
+                            activeElements[0].classList.remove("cocoslist-elements__active");
                         }
                     } catch(e) {}
                 }
             }
-        }
+        });
 
         select.parentNode.insertBefore(cocoslistElements, select.nextSibling);
 
-        for (var prop in select.getElementsByTagName('option')) {
-            if( select.hasOwnProperty( prop ) ) {
-                if(typeof select[prop].innerText != "undefined") {
-                    var list_item = document.createElement('div');
-                    list_item.setAttribute('class', 'cocoslist-element');
-                    list_item.innerText = select[prop].innerText;
+        [].forEach.call(select.getElementsByTagName('option'), function(option, index) {
+            if(typeof option.innerText != "undefined") {
+                var list_item = document.createElement('div');
+                list_item.setAttribute('class', 'cocoslist-element');
+                list_item.innerText = option.innerText;
 
-                    [].forEach.call(select[prop].attributes, function(index, attribute) {
-                        if(typeof attribute !== 'function' && typeof attribute !== 'number') {
-                            list_item.setAttribute(attribute.name, attribute.value);
+                [].forEach.call(option.attributes, function(attribute, index) {
+                    if(typeof attribute !== 'function' && typeof attribute !== 'number') {
+                        list_item.setAttribute(attribute.name, attribute.value);
+                    }
+                });
+
+                list_item.addEventListener("click", function(event) {
+                    if(this.hasAttribute("disabled") && cocoslistElements.classList.contains('cocoslist-elements__active')) {
+                        event.stopPropagation();
+                        return false;
+                    }
+
+                    if(cocoslistElements.classList.contains('cocoslist-elements__active')) {
+                        if(!cocoslistElements.querySelector('.cocoslist-element').hasAttribute("disabled")) {
+                            this.parentElement.removeChild(cocoslistElements.querySelector('.cocoslist-element'));
                         }
-                    });
 
-                    list_item.addEventListener("click", function(event) {
-                        if(this.hasAttribute("disabled") && cocoslistElements.className.indexOf('cocoslist-elements__active') > -1) {
-                            event.stopPropagation();
-                            return false;
-                        }
-                        if(cocoslistElements.className.indexOf('cocoslist-elements__active') > -1) {
-                            if(!cocoslistElements.getElementsByClassName('cocoslist-element')[0].hasAttribute("disabled")) {
-                                this.parentElement.removeChild(cocoslistElements.getElementsByClassName('cocoslist-element')[0]);
-                            }
+                        this.index = Array.prototype.slice.call( this.parentNode.children ).indexOf( this );
+                        
+                        var optionContainer     = select.querySelectorAll('option'),
+                            optionSelectedIndex = select.querySelector('[selected]').index;
 
-                            this.index = Array.prototype.slice.call( this.parentNode.children ).indexOf( this );
-     
-                            select.getElementsByTagName('option')[select.querySelectorAll('[selected]')[0].index].removeAttribute('selected');
-                            select.getElementsByTagName('option')[this.index].setAttribute("selected", "");
+                        optionContainer[optionSelectedIndex].removeAttribute('selected');
+                        optionContainer[this.index].setAttribute("selected", true);
 
-                            this.parentNode.insertBefore(this.cloneNode(true), cocoslistElements.getElementsByClassName('cocoslist-element')[0]);
-                        }
-                    });
+                        this.parentNode.insertBefore(this.cloneNode(true), cocoslistElements.querySelector('.cocoslist-element'));
+                    }
+                });
 
-                    cocoslistElements.appendChild(list_item);
-                }       
-            } 
-        }
+                cocoslistElements.appendChild(list_item);
+            }
+        });
     }
 
     this.constructor.apply(this, arguments);
 }
-
-String.prototype.trim = function(){return this.replace(/^\s+|\s+$/g, '');};
